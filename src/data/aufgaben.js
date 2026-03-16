66e731d9b516b83c12d1fd2ce68f83ae44381b28
MK <39555267+mostafakanaan@users.noreply.github.com>
2026-03-16 08:54:39 +0100

    feat: add Aufgabe 3 to overview (aufgaben.js)

--- src/data/aufgaben.js
+++ src/data/aufgaben.js
@@ -12,6 +12,7 @@ export const levels = [
     aufgaben: [
       { number: 1, title: "Aufgabe 1", link: "#" },
       { number: 2, title: "Aufgabe 2", link: "#" },
+      { number: 3, title: "Aufgabe 3", link: "/DfA/Aufgaben/3" },
     ],
   },
   {
@@ -22,6 +23,7 @@ export const levels = [
     aufgaben: [
       { number: 1, title: "Aufgabe 1", link: "#" },
       { number: 2, title: "Aufgabe 2", link: "#" },
+      { number: 3, title: "Aufgabe 3", link: "/DfA/Aufgaben/3" },
     ],
   },
   {
@@ -32,6 +34,7 @@ export const levels = [
     aufgaben: [
       { number: 1, title: "Aufgabe 1", link: "#" },
       { number: 2, title: "Aufgabe 2", link: "#" },
+      { number: 3, title: "Aufgabe 3", link: "/DfA/Aufgaben/3" },
     ],
   },
   {
@@ -42,6 +45,7 @@ export const levels = [
     aufgaben: [
       { number: 1, title: "Aufgabe 1", link: "#" },
       { number: 2, title: "Aufgabe 2", link: "#" },
+      { number: 3, title: "Aufgabe 3", link: "/DfA/Aufgaben/3" },
     ],
   },
 ];
